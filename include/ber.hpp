5898f50c26477dcc1cd4f4e83bd89fb6cff29624
DiegoSanzB <diegosanz01@gmail.com>
2024-04-29 16:42:22 -0400

    ISI BER implementation

--- include/ber.hpp
+++ include/ber.hpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <valarray>
 #include <random>
 
@@ -7,6 +8,6 @@
 const int time_unit_period = 1;
 const valarray<double> OFFSETS = {0.05, 0.1, 0.2, 0.25};
 
-valarray<double> ber_isi(CArray &pulse, int snr, int nbits, int M, float omega);
+valarray<double> ber_isi(Pulse pulse, double fs, double snr, int nbits, int M, double omega);
 
 // -- END OF FILE -- //
--- src/ber.cpp
+++ src/ber.cpp
@@ -1,18 +1,21 @@
 #include "ber.hpp"
 
-valarray<double> ber_isi(Pulse &pulse, float fs, int snr, int nbits, int M, float omega)
+valarray<double> ber_isi(Pulse pulse, double fs, double snr, int nbits, int M, double omega)
 {
-    int N = nbits/2;
-    double snr_coefficient = pow(10, snr/20);
+    int N = floor(nbits/2);
+    double snr_coefficient = pow(10, snr/20.0);
 
     valarray<double> nbits_linspace (nbits), p_isi (OFFSETS.size());
 
     for (int i=0; i <= nbits; i++)
     {
-        if (i != 0)
+        if (i < N)
         {
             // [-N, -N+1, ... , -2, -1, 1, 2, ... , N-1, N]
             nbits_linspace[i] = i-N;
+        } else
+        {
+            nbits_linspace[i] = i-N+1;
         }
     }
     for (int c = 0; c<OFFSETS.size(); c++)
@@ -23,20 +26,25 @@ valarray<double> ber_isi(Pulse &pulse, float fs, int snr, int nbits, int M, floa
         // Calculate values for gk
         for (int i=0; i<nbits; i++)
         {
-
-            gk[i] = snr_coefficient * ((int)rand() % 2 + 1) * pulse.evaluate((OFFSETS[c] - nbits_linspace[i])*time_unit_period);
+            double pm_one = ((int)rand() % 2)*2 - 1;
+            gk[i] = pm_one * snr_coefficient * pulse.evaluate((OFFSETS[c] - nbits_linspace[i])*time_unit_period);
+            // cout << "i = " << i << " -> " << nbits_linspace[i] << endl;
         }
         // Calculate the sum and product
-        double sum = 0;
-        for (int m=1; m<=M; m += 2)
+        double sum = 0.0, mult = 1.0;
+        for (double m=1.0; m<=M; m += 2.0)
         {
-            double mult = 1;
-            // Mult
             for (int k=0; k<nbits; k++) // gk.size() == nbits
             {
-                mult *= (double)(cos(m*omega*gk[k]));
+                // if (abs(m*omega*gk[k]) > lim)
+                mult = mult * cos(m*omega*gk[k]);
+                // cout << mult << endl;
+                // cout << "k = " << k << " -> " << cos(m*omega*gk[k]) << endl;
             }
-            sum += (double)((exp(-pow(m*omega, 2)/2) * sin(m*omega*g0)) / m) * mult;
+            // cout << m << " : " << mult << endl;
+            sum = sum + (exp(-0.5*pow(m*omega, 2)) * sin(m*omega*g0) / m) * mult;
+            // cout << sum << endl;
+            mult = 1;
         }
         p_isi[c] = (double)(0.5 - 2.0/PI * sum);
     }
