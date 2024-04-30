a3b7371b86f64bd042850a7ceb1d8701a54854d2
DiegoSanzB <diegosanz01@gmail.com>
2024-04-29 20:30:19 -0400

    partial CCI implementation

--- include/ber.hpp
+++ include/ber.hpp
@@ -8,6 +8,8 @@
 const int time_unit_period = 1;
 const valarray<double> OFFSETS = {0.05, 0.1, 0.2, 0.25};
 
-valarray<double> ber_isi(Pulse pulse, double fs, double snr, int nbits, int M, double omega);
+valarray<double> ber_isi(Pulse pulse, double fs, double snr, int nbits, int M);
+valarray<double> ber_cci(Pulse pulse, double fs, double snr, double sir, int L, int nbits, int M);
+valarray<double> ber_isi_cci(Pulse pulse, double fs, double snr, double sir, int L, int nbits, int M);
 
 // -- END OF FILE -- //
--- src/ber.cpp
+++ src/ber.cpp
@@ -1,17 +1,18 @@
 #include "ber.hpp"
 
-valarray<double> ber_isi(Pulse pulse, double fs, double snr, int nbits, int M, double omega)
+valarray<double> ber_isi(Pulse pulse, double fs, double snr, int nbits, int M)
 {
     int N = floor(nbits/2);
     double snr_coefficient = pow(10, snr/20.0);
+    double omega = 1 / fs;
 
     valarray<double> nbits_linspace (nbits), p_isi (OFFSETS.size());
 
     for (int i=0; i <= nbits; i++)
     {
+        // [-N, -N+1, ... , -2, -1, 1, 2, ... , N-1, N]
         if (i < N)
         {
-            // [-N, -N+1, ... , -2, -1, 1, 2, ... , N-1, N]
             nbits_linspace[i] = i-N;
         } else
         {
@@ -28,7 +29,6 @@ valarray<double> ber_isi(Pulse pulse, double fs, double snr, int nbits, int M, d
         {
             double pm_one = ((int)rand() % 2)*2 - 1;
             gk[i] = pm_one * snr_coefficient * pulse.evaluate((OFFSETS[c] - nbits_linspace[i])*time_unit_period);
-            // cout << "i = " << i << " -> " << nbits_linspace[i] << endl;
         }
         // Calculate the sum and product
         double sum = 0.0, mult = 1.0;
@@ -36,19 +36,64 @@ valarray<double> ber_isi(Pulse pulse, double fs, double snr, int nbits, int M, d
         {
             for (int k=0; k<nbits; k++) // gk.size() == nbits
             {
-                // if (abs(m*omega*gk[k]) > lim)
-                mult = mult * cos(m*omega*gk[k]);
-                // cout << mult << endl;
-                // cout << "k = " << k << " -> " << cos(m*omega*gk[k]) << endl;
+                mult *= cos(m*omega*gk[k]);
             }
-            // cout << m << " : " << mult << endl;
-            sum = sum + (exp(-0.5*pow(m*omega, 2)) * sin(m*omega*g0) / m) * mult;
-            // cout << sum << endl;
+            sum +=(exp(-0.5*pow(m*omega, 2)) * sin(m*omega*g0) / m) * mult;
             mult = 1;
         }
         p_isi[c] = (double)(0.5 - 2.0/PI * sum);
     }
     return p_isi;
+}
+
+
+valarray<double> ber_cci(Pulse pulse, double fs, double snr, double sir, int L, int nbits, int M)
+{
+    int N = floor(nbits/2);
+    double snr_coefficient = pow(10, snr/20.0);
+    double sir_coefficient = pow(10, sir/20.0);
+    double omega = 1 / fs;
+
+    valarray<double> nbits_linspace (nbits), p_isi (OFFSETS.size());
+
+    for (int i=0; i <= nbits; i++)
+    {
+        // [-N, -N+1, ... , -2, -1, 1, 2, ... , N-1, N]
+        if (i < N)
+        {
+            nbits_linspace[i] = i-N;
+        } else
+        {
+            nbits_linspace[i] = i-N+1;
+        }
+    }
+    for (int c = 0; c<OFFSETS.size(); c++)
+    {
+        double g0 = snr_coefficient*pulse.evaluate(OFFSETS[c]*time_unit_period);
+
+        // Calculate r_i assuming each co-channel has same power
+        double r_i = sqrt(g0*g0/(L*sir_coefficient));   // TODO: coefficient seems off given P_CCI values in paper
+
+        // Calculate the sum and product
+        double sum = 0.0, mult = 1.0;
+        for (double m=1.0; m<=M; m += 2.0)
+        {
+            for (int i=1; i<=L; i++) // L co-channels
+            {
+                mult *= cyl_bessel_j(0, (double)(m*omega*r_i));
+            }
+            sum += (exp(-0.5*pow(m*omega, 2)) * sin(m*omega*g0) / m) * mult;
+            mult = 1;
+        }
+        p_isi[c] = (double)(0.5 - 2.0/PI * sum);
+    }
+    return p_isi;
+}
+
+
+valarray<double> ber_isi_cci(Pulse pulse, double fs, double snr, double sir, int L, int nbits, int M)
+{
+    // TODO: implement isi + cci
 }
 
 // -- END OF FILE -- //
